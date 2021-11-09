6301b293e52c50b2f07724f542317da142db70e8
Arthur Dibe <dibearthur@gmail.com>
2021-11-09 11:56:51 -0500

    Included header file for tools module

---
+++ sample_graphs_app/tools.h
@@ -0,0 +1,16 @@
+/********************************
+Developed By: DIBE
+********************************/
+
+#ifndef NAMESPACE_TOOLS_H 
+#define NAMESPACE_TOOLS_H
+
+namespace sdds 
+{
+	// Displays the user interface menu
+	int menu();
+
+	// Performs a fool-proof integer entry
+	int getInt(int min, int max);
+}
+#endif
